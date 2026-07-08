0e5df043c2e410f9a036e57d72ae8848399dae3a
SurenChandraSekaran <147575172+SurenChandraSekaran@users.noreply.github.com>
2026-07-08 22:02:30 +0800

    Update livewire.php

--- stego-messaging-app/config/livewire.php
+++ stego-messaging-app/config/livewire.php
@@ -64,9 +64,9 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'disk' => 'local',        // Example: 'local', 's3'              | Default: 'default'
         'rules' => ['required', 'file', 'max:256000'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'directory' => 'livewire-tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => 'throttle:300,1',  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
